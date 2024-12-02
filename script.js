b9c57f96cb7c9b99381bc5a9251c2c6b3f4a0776
Guillaume Lévesque <barouet@gmail.com>
2024-12-02 15:25:08 -0500

    initial

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('play-audio-1').addEventListener('click', () => {
+    const audio1 = new Audio('noise.mp3');
+    audio1.play();
+  });
+  
+  document.getElementById('play-audio-2').addEventListener('click', () => {
+    const audio2 = new Audio('noise.mp3');
+    audio2.play();
+  });
--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,25 @@
-const CACHE_NAME = 'beaulne-player-cache-v2';
-const ASSETS = [
-    '/beaulne-player/index.html',
-    '/beaulne-player/manifest.json',
-    '/beaulne-player/service-worker.js',
-    '/beaulne-player/audio/noise.mp3',
-    '/beaulne-player/styles.css',
-    '/beaulne-player/script.js'
+const CACHE_NAME = 'audio-player-cache';
+const assetsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './noise.mp3',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// Install event - cache all assets
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assetsToCache))
+  );
 });
 
-// Fetch event - serve cached files if available
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
-});
-
-// Activate event - clear old caches
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
